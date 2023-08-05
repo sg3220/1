b38d1d9195a50570cdc733fa2a88c12901a1f60e
Siddharth Ghosh <email>
2023-08-05 11:59:19 +0530

    Added Accout, Cart, Home

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./Components/Header";
+
 import Home from "./Components/Home";
+import Account from "./Components/Account";
+import Cart from "./Components/Cart";
+
 import Footer from "./Components/Footer";
+
 import "./Styles/App.scss";
+import "./Styles/Home.scss";
 import "./Styles/Mixins.scss";
 import "./Styles/Utilities.scss";
 
@@ -14,6 +20,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Account" element={<Account />} />
+        <Route path="/Cart" element={<Cart />} />
       </Routes>
       <Footer />
     </Router>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./../Styles/Utilities.scss";
+
 const Home = () => {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <div className="ImageSection">ImageSection</div>
+      <div className="TextSection">TextSection</div>
+    </div>
+  );
 };
 
 export default Home;
